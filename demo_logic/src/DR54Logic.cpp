20ec7519e2d1e2a60ee8cb0ed17d27fb30d91c44
Carlos J. Rosales <cjrosales@gmail.com>
2015-11-22 23:14:37 +0100

    * Updates the logic with new states, and less manual input. * Implements moveCloser and moveAway tasks. * Changes the internal representation of the exploration sample for several touch poses.

--- demo_logic/src/DR54Logic.cpp
+++ demo_logic/src/DR54Logic.cpp
@@ -7,8 +7,10 @@
 #include <std_srvs/Empty.h>
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
+#include <geometry_msgs/PoseArray.h>
 #include <trajectory_msgs/JointTrajectory.h>
 #include <actionlib/client/simple_action_client.h>
+#include <kdl_conversions/kdl_msg.h>
 
 // moveit
 // Important: these need to be included before the decision_making includes
@@ -40,8 +42,9 @@ using namespace decision_making;
 // IMPORTANT: points must become trajectories or curves in the future
 
 // from object model to exploration
-geometry_msgs::PointStamped sampledPoint;
-geometry_msgs::Vector3Stamped sampledNormal;
+// generate equaly distributed 16 poses with z-axis aligned to normal, and differnt x- and y-axes
+std::vector<geometry_msgs::PoseStamped> normal_aligned_targets;
+int n_div = 16;
 
 // from exploration to model update
 geometry_msgs::PointStamped touchedPoint;
@@ -53,6 +56,9 @@ ros::Publisher talker;
 trajectory_msgs::JointTrajectory openHand;
 trajectory_msgs::JointTrajectory closeHand;
 ros::Publisher hand_commander;
+
+// only needed for visual debug
+ros::Publisher pose_array_pub;
 
 //////////////////////////////////////////////////////
 //////////////////      TASKs       //////////////////
@@ -68,7 +74,6 @@ decision_making::TaskResult homeTask(string name, const FSMCallContext& context,
     // configure the groups
     moveit::planning_interface::MoveGroup two_group("two_arms");
     moveit::planning_interface::MoveGroup left_group("left_arm_hand");
-    moveit::planning_interface::MoveGroup right_group("right_arm_probe");
 
     // configure the home moves
     two_group.setNamedTarget("two_arms_home");
@@ -246,6 +251,7 @@ decision_making::TaskResult createModelTask(string name, const FSMCallContext& c
     std::string create_model_srv_name = "/gaussian_process/start_process";
     gp_regression::start_process create_model_srv;
     create_model_srv.request.cloud_dir = "";
+    // create_model_srv.request.cloud_dir = "/home/pacman/Projects/catkin_ws/src/pacman-DR54/gaussian-object-modelling/resources";
 
     // call the service
     if( !(ros::service::call( create_model_srv_name, create_model_srv) ))
@@ -280,25 +286,131 @@ decision_making::TaskResult generateTrajectoryTask(string name, const FSMCallCon
     }
 
     // convert to the exchange data type
-    // TEMP: point and vector should be substituted by a full trajectory or curve
+    // TEMP: std::vector<geometry_msgs::PoseStamped> normal_aligned_targets;
+    geometry_msgs::PointStamped sampledPoint;
+    geometry_msgs::Vector3Stamped sampledNormal;
     sampledPoint.header = sample_model_srv.response.trajectory.header;
     sampledPoint.point = sample_model_srv.response.trajectory.point;
     sampledNormal.header = sample_model_srv.response.trajectory.header;
     sampledNormal.vector = sample_model_srv.response.trajectory.direction;
+    // this value should be provided by the gaussian process, the safety distance is inversely proportional
+    // to the certainty of the point.
+    // ATTENTION: here it is assumed that normal is the outward normal
+    double safety_distance = 0.05;
 
-    cout << sampledNormal << endl;
+    // The whole exploration strategy should be provided by the smart policy
+    // For the moment, we do the following:
+    // Create the reference frame
+    KDL::Vector o_ref(sampledPoint.point.x, sampledPoint.point.y, sampledPoint.point.z);
+    KDL::Vector z_ref(sampledNormal.vector.x, sampledNormal.vector.y, sampledNormal.vector.z);
+    z_ref.Normalize();
+    o_ref = o_ref - safety_distance*z_ref;
+    KDL::Vector y_ref(0.0, 1.0, 0.0);
+    KDL::Vector x_ref = y_ref*z_ref;
+    x_ref.Normalize();
+    y_ref = z_ref*x_ref;
+    y_ref.Normalize();
+    KDL::Rotation q_ref(x_ref, y_ref, z_ref);
+    KDL::Frame g_ref(q_ref, o_ref);
+
+    // only needed for visual debug
+    geometry_msgs::PoseArray normal_aligned_array;
+    normal_aligned_array.header = sampledPoint.header;
+
+    for(int i = 0; i < n_div; ++i)
+    {
+        double current_angle = ((double)i/(double)n_div)*6.283185307179586; //2*pi
+        KDL::Rotation current_orientation;
+        current_orientation.DoRotZ(current_angle);
+        KDL::Frame current_frame (current_orientation);
+        current_frame = g_ref*current_frame;
+
+        // convert to geometry msg
+        geometry_msgs::PoseStamped current_target;
+        tf::poseKDLToMsg(current_frame, current_target.pose);
+        current_target.header = sampledPoint.header;
+        normal_aligned_targets.push_back(current_target);
+
+        // only needed for visual debug
+        normal_aligned_array.poses.push_back(current_target.pose);
+    }
+
+    // only needed for visual debug
+    pose_array_pub.publish(normal_aligned_array);
 
     eventQueue.riseEvent("/PolicyGenerated");
 
     return TaskResult::SUCCESS();
 }
-/*
-decision_making::TaskResult explorationTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+
+decision_making::TaskResult moveCloserTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
 {
+    ROS_INFO("Approaches to the surface according to exploration policy...");
+
+    // configure, set targets and work frames, and move the group
+    moveit::planning_interface::MoveGroup touch_group("touch_chain");
+    touch_group.setEndEffector("touch");
+    touch_group.setPoseTargets(normal_aligned_targets, touch_group.getEndEffectorLink());
+
+    if( !(touch_group.move()==moveit_msgs::MoveItErrorCodes::SUCCESS) )
+    {
+        ROS_ERROR("An error occured approaching to surface. Coming back to object modelling...");
+        eventQueue.riseEvent("/DidNotExplore");
+        return TaskResult::TERMINATED();
+    }
 
     return TaskResult::SUCCESS();
 }
 
+decision_making::TaskResult moveAwayTask(string name, const FSMCallContext& context, EventQueue& eventQueue)
+{
+    ROS_INFO("Moves away from the surface according to exploration policy...");
+
+    // this value should be provided by the gaussian process, the safety distance is inversely proportional
+    // to the certainty of the point.
+    // ATTENTION: here it is assumed that z always points toward the surface
+    double safety_distance = 0.05;
+
+    // configure, set targets and work frames, and move the group
+    moveit::planning_interface::MoveGroup touch_group("touch_chain");
+    touch_group.setEndEffector("touch");
+    geometry_msgs::PoseStamped retreat_target = touch_group.getCurrentPose(touch_group.getEndEffectorLink());
+    KDL::Frame retreat_pose;
+    tf::poseMsgToKDL(retreat_target.pose, retreat_pose);
+    retreat_pose.p = retreat_pose.p - safety_distance*retreat_pose.M.UnitZ();
+    tf::poseKDLToMsg(retreat_pose, retreat_target.pose);
+
+    touch_group.setPoseTarget(retreat_target, touch_group.getEndEffectorLink());
+
+    if( !(touch_group.move()==moveit_msgs::MoveItErrorCodes::SUCCESS) )
+    {
+        ROS_ERROR("An error occured moving away from surface. Coming back to object modelling...");
+        eventQueue.riseEvent("/DidNotExplore");
+        return TaskResult::TERMINATED();
+    }
+
+    ROS_INFO("Now, take probe to home...");
+
+    // configure the group
+    moveit::planning_interface::MoveGroup right_group("right_arm_probe");
+
+    // configure the home move
+    right_group.setNamedTarget("right_arm_home");
+
+    // call the move
+    if( !(right_group.move()==moveit_msgs::MoveItErrorCodes::SUCCESS) )
+    {
+        ROS_ERROR("An error occured moving the probe to home. But exploration was succesful, retry moving away again...");
+        eventQueue.riseEvent("/DoneExploration");
+        return TaskResult::TERMINATED();
+    }
+
+    eventQueue.riseEvent("/FinishedExploration");
+
+    return TaskResult::SUCCESS();
+}
+
+/*
 decision_making::TaskResult emergencyStop(string name, const FSMCallContext& context, EventQueue& eventQueue)
 {
 
@@ -356,15 +468,31 @@ FSM(ObjectModelling)
     {
         FSM_STATE(Query)
         {
+            // When a measure of completeness is available, everytime we enter
+            // this state, we ask if the model is complete using a given
+            // threshold on the measure
+            // this task is the only one to raise the event /ModelCompleted
+            // to finish the demo
+            // FSM_CALL_TASK(checkModelCompleteness);
+
             FSM_TRANSITIONS
             {
+                // Manual queries
                 FSM_ON_EVENT("/GenerateModel", FSM_NEXT(GaussianModel));
                 FSM_ON_EVENT("/GeneratePolicy", FSM_NEXT(ExplorationStrategy));
                 FSM_ON_EVENT("/UpdateModel", FSM_NEXT(UpdateModel));
                 FSM_ON_EVENT("/QueryDone", FSM_RAISE("/ObjectModellingDone"));
 
+                // Auto queries
+                // In case it all went well with the exploration
+                FSM_ON_EVENT("/FinishedExploration", FSM_NEXT(UpdateModel));
+
+                // In case something went wrong with the exploration, change strategy
+                FSM_ON_EVENT("/DidNotExplore", FSM_NEXT(ExplorationStrategy));
+
                 // Once the exploration measure gives that the object is complete
-                FSM_ON_EVENT("/ModelCompleted", FSM_RAISE("/GoHome"));
+                // return the object, for now, go home.
+                FSM_ON_EVENT("/ModelCompleted", FSM_RAISE("/ReturnObject"));
             }
         }
         FSM_STATE(GaussianModel)
@@ -378,7 +506,7 @@ FSM(ObjectModelling)
         }
         FSM_STATE(UpdateModel)
         {
-            ROS_WARN("Right now, it does automatically when published in clicked point, but not he best...");
+            ROS_WARN("Enters here automatically after exploration...");
 
             FSM_TRANSITIONS
             {
@@ -402,74 +530,56 @@ FSM(TactileExploration)
 {
     FSM_STATES
     {
-        CheckPolicy,
         ApproachToSurface,
-        TouchSurface,
-        FollowPolicy,
+        TactileExploration,
         RetreatFromSurface
     }
-    FSM_START(CheckPolicy);
+    FSM_START(ApproachToSurface);
     FSM_BGN
     {
-        FSM_STATE(CheckPolicy)
-        {
-            ROS_INFO("Plan kinematically the whole trajectory and check validity");
-
-            FSM_TRANSITIONS
-            {
-                FSM_ON_EVENT("/GoodTrajectory", FSM_NEXT(ApproachToSurface));
-                FSM_ON_EVENT("/BadTrajectory", FSM_RAISE("/DidNotExplore"));
-            }
-        }
         FSM_STATE(ApproachToSurface)
         {
-            // Move in Cartesian impedance control in free space close to the surface with:
+            // Move with position control and planning, in free space, close to the surface with:
             //    * The z-axis of tip parallel to surface normal at desired contact point
-            //    * Displace according to model uncertainty along z-axis
-            //    * With high-stiffness in all axes
+            //    * Displace according to model uncertainty along z-axis ?
 
-            ROS_INFO("Engage free space controller and send command");
+            FSM_CALL_TASK(moveCloser);
 
             FSM_TRANSITIONS
             {
-                FSM_ON_EVENT("/CloseToSurface", FSM_NEXT(TouchSurface));
+                FSM_ON_EVENT("/CloseToSurface", FSM_NEXT(TactileExploration));
             }
         }
-        FSM_STATE(TouchSurface)
+        FSM_STATE(TactileExploration)
         {
             // Move using force impedance control using:
             //    * ~5N in z-axis of tip (depends on the sensor capabilities)
             //    * With low-stiffness in z-axis, the rest are stiff
+            // FSM_CALL_TASK(touchSurface);
 
             ROS_INFO("Engage touching controller and send command, and raise event when touching");
 
-            FSM_TRANSITIONS
-            {
-                FSM_ON_EVENT("/Touching", FSM_NEXT(FollowPolicy));
-            }
-        }
-        FSM_STATE(FollowPolicy)
-        {
             // Move in Cartesian impedance control with:
             //    * ~5N in z-axis of tip
             //    * With orientation correction to keep z-axis of tip parallel to surface normal
             //    * With low-stiffness in z-axis, the rest are stiff
             //    * xy coordinates follow the trajectory over the surface
+            // FSM_CALL_TASK(followPolicy);
 
             ROS_INFO("Engage contour following controller and send command, and raise event after completion");
 
-            ROS_INFO("[MATLAB] After completion, save the exploration data such that the model can be updated");
-
             FSM_TRANSITIONS
             {
-                FSM_ON_EVENT("/FinishedRun", FSM_NEXT(RetreatFromSurface));
+                FSM_ON_EVENT("/DoneExploration", FSM_NEXT(RetreatFromSurface));
             }
         }
         FSM_STATE(RetreatFromSurface)
         {
-            // Move in Cartesian impedance control with:
-            //    * Move away in the z-axis until no contact (~0N along z-axis)
+            // with position control and planning, away from the surface
+            //    * Move away in the z-axis (normal direction) until no contact (~0N along z-axis)
             //    * With high stiffness in all direction return to home position
+
+            FSM_CALL_TASK(moveAway);
 
             ROS_INFO("Engage no-contact controller and send commands");
 
@@ -477,7 +587,6 @@ FSM(TactileExploration)
             {
                 FSM_ON_EVENT("/AwayFromSurface", FSM_RAISE("/FinishedExploration"));
             }
-
         }
     }
     FSM_END
@@ -495,7 +604,8 @@ FSM(DR54Logic)
         Home,
         Start,
         ObjectModelling,
-        TactileExploration
+        TactileExploration,
+        End
     }
     FSM_START(Off);
     FSM_BGN
@@ -535,8 +645,8 @@ FSM(DR54Logic)
             FSM_TRANSITIONS
             {
                 FSM_ON_EVENT("/EStop", FSM_NEXT(Off));
-                FSM_ON_EVENT("/ObjectModellingDone", FSM_NEXT(TactileExploration));
-                FSM_ON_EVENT("/GoHome", FSM_NEXT(Home));
+                FSM_ON_EVENT("/Explore", FSM_NEXT(TactileExploration));
+                FSM_ON_EVENT("/ReturnObject", FSM_NEXT(End));
             }
         }
         FSM_STATE(TactileExploration)
@@ -549,7 +659,16 @@ FSM(DR54Logic)
                 FSM_ON_EVENT("/DidNotExplore", FSM_NEXT(ObjectModelling));
             }
         }
+        FSM_STATE(End)
+        {
+            // FSM_CALL_TASK(returnObject)
 
+            FSM_TRANSITIONS
+            {
+                FSM_ON_EVENT("/EStop", FSM_NEXT(Off));
+                FSM_ON_EVENT("/ObjectReturned", FSM_NEXT(Home));
+            }
+        }
     }
     FSM_END
 }
@@ -569,8 +688,11 @@ int main(int argc, char** argv){
     // sound client
     talker = nodeHandle.advertise<sound_play::SoundRequest>("/robotsound", 5);
 
+    // only for visual debug
+    pose_array_pub = nodeHandle.advertise<geometry_msgs::PoseArray>("/normal_aligned_targets", 16, true);
+
     // useful hand and close commands in 1sec and publisher
-    // ToDo: move it to moveit! to use the move command
+    // ToDo: move it with moveit! to use the move command
     openHand.joint_names.push_back("left_hand_synergy_joint");
     closeHand.joint_names.push_back("left_hand_synergy_joint");
     trajectory_msgs::JointTrajectoryPoint p;
@@ -587,6 +709,8 @@ int main(int argc, char** argv){
     LocalTasks::registrate("recognizeHand", handTask);
     LocalTasks::registrate("createModel", createModelTask);
     LocalTasks::registrate("toExploreTrajectory", generateTrajectoryTask);
+    LocalTasks::registrate("moveCloser", moveCloserTask);
+    LocalTasks::registrate("moveAway", moveAwayTask);
 
     // 3: Go!
     ros::AsyncSpinner spinner(2);
